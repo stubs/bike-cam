453e7eed43ba8eaea6582b9c9c0eedaddd4067e1
Aaron Gonzalez <a.gonzalez979@gmail.com>
2022-05-27 22:23:38 -0400

    removed shell script

--- bike_cam_rec.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-python "$SCRIPT_DIR"/capture.py --dt $(date -I) --hour $(date "+%H") --min $(date "+%M")
